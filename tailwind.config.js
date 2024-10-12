906a9e8baed632b6cf3edf47a0b4d120d97878c8
Baliqis <sulaimabaliqis@gmail.com>
2024-10-12 07:03:59 +0100

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-/** @type {import('tailwindcss').Config} */
->>>>>>> 01c5cb835cfd070b5552526d566792c6a45d1a2f
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
